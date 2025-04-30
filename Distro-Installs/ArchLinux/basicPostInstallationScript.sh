923fff2a577d33c6ad373f54f2a141cc6de35488
Haerbernd <jan@schmidt-oc.de>
2025-04-30 14:52:10 +0200

    Added debug messages (echo) to the basicPostInstallScript and added a script to install and add hypr-ecosystem programs

--- Distro-Installs/ArchLinux/basicPostInstallationScript.sh
+++ Distro-Installs/ArchLinux/basicPostInstallationScript.sh
@@ -6,17 +6,20 @@ sudo pacman -S --needed networkmanager nm-connection-editor network-manager-appl
 sudo pacman -S --needed base-devel git # Install yay requirements
 sudo pacman -S --needed flatpak fastfetch alacritty duf htop samba curl firefox firefox-developer-edition thunderbird gnupg # Misc stuff
 
+echo "Start installation of yay..."
 # Installation of yay
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si  # Follow instructions on screen
 yay --version # Check installation of yay
+echo "yay was successfully installed..." && echo ""
 
 yay -Syu
 yay -S --needed mkinitcpio-numlock
-echo "Don't forget to add ´numlock´ in ´/etc/mkinitcpio.conf´ as described in ´https://wiki.archlinux.org/title/Activating_numlock_on_bootup´"
+echo "Don't forget to add ´numlock´ in ´/etc/mkinitcpio.conf´ as described in ´https://wiki.archlinux.org/title/Activating_numlock_on_bootup´" && echo ""
 
+echo "Start installation of Hyprland"
 # Hyprland installation
 sudo pacman -S --needed hyprland uwsm wofi dolphin
 sudo pacman -S --needed gtk4 rust greetd # Installs a login manager (greetd)
@@ -30,3 +33,12 @@ curl "" -o ~/.local/bin/make-screenshot.sh && chmod +x ~/.local/bin/make-screens
 sudo pacman -S --needed mako pipewire wireplumber qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk hyprpolkitagent
 curl "" -o ~/.config/hypr/xdph.conf
 systemctl --user enable --now hyprpolkitagent.service
+echo "Hyprland was successfully installed..." && echo ""
+
+echo "Start installing further systems of the Hypr-Ecosystem"
+curl "" -o ~/hyprEcosystemSetup.sh
+~./hyprEcosystemSetup.sh
+echo "Hypr-Ecosystem was successfully installed..."
+echo "Start removing installation script..."
+rm hyprEcosystemSetup.sh
+echo "done..." && echo ""
