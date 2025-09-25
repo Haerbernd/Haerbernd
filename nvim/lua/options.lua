bca537775cc06561797f30b567012fea6cb7f42c
Haerbernd <105395307+Haerbernd@users.noreply.github.com>
2025-09-25 13:48:42 +0200

    Create options.lua

---
+++ nvim/lua/options.lua
@@ -0,0 +1,21 @@
+-- Tabs
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- UI config
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright
+vim.opt.termguicolors = true -- enable 24-bit RGB color in the TUI
+vim.opt.showmode = true -- set to false if you don't want to see the modes (e.g. "-- INSERT --")
+
+-- Searching
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
